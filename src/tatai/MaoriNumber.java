d03c91f859c70a8984176fa89cfb72ce8004c290
darcycox97 <darcycox97@gmail.com>
2017-09-13 22:31:06 +1200

    Made a general Number class. Should program to this rather than MaoriNumber. Improves extensibility.

--- src/tatai/MaoriNumber.java
+++ src/tatai/MaoriNumber.java
@@ -8,44 +8,40 @@ import java.io.IOException;
 /**
  * Class to encapsulate information about a maori number.
  */
-public class MaoriNumber {
-	private static final File dictionary = new File("resources/dictionary.txt");
-
-	private String text; // the maori spelling of the number
-	private int value; // the actual value of the number
+public class MaoriNumber extends Number {
+	
+	private static final File dictionary = new File("resources/dictionary.txt"); // the file containing spelling of maori numbers from 1 to 10.
 
 	public MaoriNumber(int value) {
-		this.value = value;
-		text = getSpelling(value);
+		super(value);
 	}
 
-	public int getValue() {
-		return value;
-	}
-	
-	public String toString() {
-		return text;
-	}
-
-	// returns the string representation of this number in maori spelling
-	private String getSpelling(int value) {
+	/**
+	 * This implementation constructs the maori spelling of the number.
+	 */
+	protected String getSpelling(int value) {
 
 		if (value <= 10) {
-			return getSpellingOfNumber(value);
+			return getSpellingOfNumberFrom1to10(value);
 		} else {
 			// get first digit
 			int firstDigit = Integer.parseInt(String.valueOf(value).substring(0,1));
 			int secondDigit = Integer.parseInt(String.valueOf(value).substring(1,2));
 			
 			if (firstDigit == 1) {
-				return "tekau ma " + getSpellingOfNumber(value);
+				return "tekau ma " + getSpellingOfNumberFrom1to10(value);
 			} else {
-				return getSpellingOfNumber(firstDigit) + " tekau ma " + getSpellingOfNumber(secondDigit);
+				return getSpellingOfNumberFrom1to10(firstDigit) + " tekau ma " + getSpellingOfNumberFrom1to10(secondDigit);
 			}
 		}
 	}
 
-	private String getSpellingOfNumber(int num) {
+	/**
+	 * Helper method to determine the spelling of a maori number from one to ten. 
+	 * The nature of maori numbers allow us to construct the spelling of any number using 
+	 * the first 10 numbers.
+	 */
+	private String getSpellingOfNumberFrom1to10(int num) {
 
 		String line;
 		BufferedReader reader;
--- src/tatai/Number.java
+++ src/tatai/Number.java
@@ -1,5 +1,28 @@
 package tatai;
 
-public class Number {
-
+/**
+ * Defines general behaviour of numbers in this application. Numbers could be in any language, it is up to sub-classes to determine 
+ */
+public abstract class Number {
+	private String text; // the string representation of the number (as a word) e.g 1 = "one" in English
+	private int value; // the actual value of the number
+	
+	public Number(int value) {
+		this.value = value;
+		this.text = getSpelling(value);
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public String toString() {
+		return text;
+	}
+	
+	/**
+	 * To be implemented by subclasses, defines how to spell any number.
+	 * @param value The number to get the spelling of
+	 */
+	protected abstract String getSpelling(int value);
 }
