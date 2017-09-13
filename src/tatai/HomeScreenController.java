efcac7db2d01df1ac092c3b041fd9b7edd984dab
Darcy Cox <darcycox97@gmail.com>
2017-09-13 16:02:11 +1200

    Figured out switching screens

--- src/tatai/HomeScreenController.java
+++ src/tatai/HomeScreenController.java
@@ -1,18 +1,31 @@
 package tatai;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 
 /**
  *	Defines the event handlers for the controls on the Tatai home screen.
  */
 public class HomeScreenController {
+	
 	@FXML
 	private Button btn1to9;
 	
 	@FXML
 	public void btnClicked() {
-		btn1to9.setText("Changed");
+		BorderPane root;
+		try {
+			root = (BorderPane)FXMLLoader.load(getClass().getResource("GameScreen.fxml"));
+			btn1to9.getScene().setRoot(root);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	
