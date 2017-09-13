4db371a703cd53ed3e9dddcb24e4e01e102189e9
Molly Farrant <mfar672@aucklanduni.ac.nz>
2017-09-13 16:15:34 +1200

    Added return home button

--- src/tatai/GameScreenController.java
+++ src/tatai/GameScreenController.java
@@ -1,5 +1,28 @@
 package tatai;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
 public class GameScreenController {
 
+	@FXML
+	private Button returnHome;
+	
+	@FXML
+	public void homeClicked() {
+		BorderPane root;
+		try {
+			root = (BorderPane)FXMLLoader.load(getClass().getResource("HomeScreen.fxml"));
+			returnHome.getScene().setRoot(root);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
