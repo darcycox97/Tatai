af4e34cd84e6b884af5abec84c26c9ad48956de4
darcycox97 <darcycox97@gmail.com>
2017-10-09 22:30:01 +1300

    Basic game menu added

---
+++ src/tatai/GameMenuController.java
@@ -0,0 +1,20 @@
+package tatai;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Tooltip;
+
+public class GameMenuController {
+	
+	@FXML private ToggleGroup toggleDifficulty;
+	@FXML private RadioButton toggleEasy;
+	@FXML private RadioButton toggleHard;
+	@FXML private Tooltip tooltipTest;
+	@FXML private Label lblHelp;
+
+	public void initialize() {
+		//lblHelp.setTooltip(new Tooltip("Testing"));
+	}
+}
