069a17f7dddb6c54fc7fdc6328bcc766a383d8fe
darcycox97 <darcycox97@gmail.com>
2017-10-13 00:55:33 +1300

    Finished validation checks on custom quiz screen. Now successfully writes quizzes to the csv file

--- src/tatai/controller/QuizCreatorController.java
+++ src/tatai/controller/QuizCreatorController.java
@@ -7,16 +7,29 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class QuizCreatorController {
 
 	private static final int NUM_EQUATIONS = 10;
 	
 	private final File quizCSV = new File("resources/quizzes.csv");
+	
+	// map to be used to check validity of each equation field
+	private Map<Integer, Boolean> validityMap;
 
 	@FXML private TextField eqn1;
 	@FXML private TextField eqn2;
@@ -29,6 +42,18 @@ public class QuizCreatorController {
 	@FXML private TextField eqn9;
 	@FXML private TextField eqn10;
 	Map<Integer,TextField> eqnMap = new HashMap<Integer,TextField>();
+	
+	@FXML private Label lbl1;
+	@FXML private Label lbl2;
+	@FXML private Label lbl3;
+	@FXML private Label lbl4;
+	@FXML private Label lbl5;
+	@FXML private Label lbl6;
+	@FXML private Label lbl7;
+	@FXML private Label lbl8;
+	@FXML private Label lbl9;
+	@FXML private Label lbl10;
+	Map<Integer,Label> lblMap = new HashMap<Integer,Label>();
 	
 	@FXML private Button btnGoBack;
 	@FXML private Button btnSaveQuiz;
@@ -49,31 +74,92 @@ public class QuizCreatorController {
 		eqnMap.put(9, eqn9);
 		eqnMap.put(10, eqn10);
 		
+		// initialize hash map of labels
+		lblMap.put(1, lbl1);
+		lblMap.put(2, lbl2);
+		lblMap.put(3, lbl3);
+		lblMap.put(4, lbl4);
+		lblMap.put(5, lbl5);
+		lblMap.put(6, lbl6);
+		lblMap.put(7, lbl7);
+		lblMap.put(8, lbl8);
+		lblMap.put(9, lbl9);
+		lblMap.put(10, lbl10);
+		
+		validityMap = new HashMap<Integer,Boolean>();
+		
 		for (int i = 1; i <= NUM_EQUATIONS; i++) {
-			//TODO: implement a listener for the textfields
-			// eqnMap.get(i).addListener // listen to each text field to check for valid input
+			// initialize all equations to be invalid
+			validityMap.put(i, false);
+			
+			// listen to each text field to check for valid input
+			TextField addTo = eqnMap.get(i);
+			addTo.textProperty().addListener(new EquationValidifier(i));
 		}
 	}
 	
 	@FXML public void loadTeacherMenu() {
-		//TODO: 
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/TeacherMenu.fxml"));
+			Parent root = loader.load();
+			btnGoBack.getScene().setRoot(root);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@FXML
 	public void saveQuiz() {
-		// the save quiz button will only become enabled when all text fields contain a valid equation.
-		// this means no checks need to be done at this point.
 		
-		// convert quiz into its csv form.
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append(txtQuizName.getText()); // first entry should be the quiz name
-		
+		boolean validFields = true;
 		for (int i = 1; i <= NUM_EQUATIONS; i++) {
-			sb.append("," + eqnMap.get(i).getText());
+			if (!validityMap.get(i)) {
+				// not valid if one or more equations arent valid
+				validFields = false;
+			}
 		}
 		
-		addQuizToCSV(sb.toString());
+		String name = txtQuizName.getText();
+		if (!Pattern.matches("[\\w\\-]+", name)) {
+			// only allow alphanumeric, hyphens, and underscores
+			validFields = false;
+		}
+		
+		if (validFields) {
+			// convert quiz into its csv form.
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append(txtQuizName.getText()); // first entry should be the quiz name
+			
+			for (int i = 1; i <= NUM_EQUATIONS; i++) {
+				sb.append("," + eqnMap.get(i).getText().replaceAll(" ", ""));
+			}
+			
+			addQuizToCSV(sb.toString());
+			
+			Alert success = new Alert(
+				AlertType.INFORMATION,
+				"Quiz saved!"
+			);
+			success.showAndWait();
+			
+			// clear existing text fields and graphics
+			txtQuizName.setText("");
+			for (int i = 1; i <= NUM_EQUATIONS; i++) {
+				eqnMap.get(i).setText("");
+				lblMap.get(i).setGraphic(null);
+			}
+			
+		} else {
+			// prompt user to provide valid input
+			Alert info = new Alert(
+				AlertType.INFORMATION,
+				"You must provide ten valid equations \nand a valid quiz name to save this quiz."
+			);
+			info.showAndWait();
+		}
+		
 	}
 	
 	/**
@@ -97,6 +183,86 @@ public class QuizCreatorController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private class EquationValidifier implements ChangeListener<String> {
+		
+		private int observed; // the text field to check input of
+
+		public EquationValidifier(int eqnNumber) {
+			observed = eqnNumber;
+		}
+		
+		@Override
+		public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+			
+			boolean validEquation = false;
+			
+			// remove spaces from the equation so it is of the form a+b
+			String equation = newValue.replace(" ", "");
+			
+			// check that equation consists of two numbers either side of either "+", "-", or "x".
+			
+			int i = 0;
+			while (i < equation.length() && Character.isDigit(equation.charAt(i))) {
+				i++;
+			}
+			
+			if (i != equation.length() && i != 0) {
+				
+				// i points to the index of the first non digit character. 
+				int operand1 = Integer.parseInt(equation.substring(0, i));
+
+				// check that first non digit character is a valid operation
+				char operation = equation.charAt(i);
+				if (operation == 'x' || operation == '+' || operation == '-') {
+	
+					// all good so far, check last part of equation is also a digit
+					i++;
+					
+					int operand2Index = i;
+					
+					while (i < equation.length() && Character.isDigit(equation.charAt(i))) {
+						i++;
+					}
+					
+					if (i == equation.length() && operand2Index != equation.length()) {
+						// remaining characters all digits, check answer is from 1 to 99.
+						int operand2 = Integer.parseInt(equation.substring(operand2Index, equation.length()));
+						
+						int answer;
+						if (operation == '+') {
+							answer = operand1 + operand2;
+						} else if (operation == 'x') {
+							answer = operand1 * operand2;
+						} else {
+							answer = operand1 - operand2;
+						}
+						
+						if (answer >= 1 && answer <= 99) {
+							validEquation = true;
+						}
+						
+					}
+				}
+				
+			}
+			
+			if (validEquation) {
+				// display tick next to text field
+				lblMap.get(observed).setGraphic(new ImageView(new Image(getClass().getResourceAsStream("../view/icons/tick.png"),25,25,true,true)));
+				validityMap.put(observed, true); // update validity of this text field
+			} else {
+				// display cross next to text field
+				lblMap.get(observed).setGraphic(new ImageView(new Image(getClass().getResourceAsStream("../view/icons/cross.png"),25,25,true,true)));
+				validityMap.put(observed, false); // update validity of this text field
+			}
+			
+			
+			
+		}
+			
+		
 	}
 	
 }
